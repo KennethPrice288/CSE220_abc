622a984ea824b6adeacdeb97db28578aedeac707
Kenneth Price <krprice@ucsc.edu>
2023-11-16 16:38:01 -0800

    Fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 make libabc.a ABC_USE_NO_READLINE=1
-riscv64_linux_gnu_gcc -static -Wall -g -c test.c -o test.o
-riscv64_linux_gnu_g++ -static -g -o test test.o libabc.a -lm -ldl -lpthread
+riscv64-linux-gnu-gcc -static -Wall -g -c test.c -o test.o
+riscv64-linux-gnu-g++ -static -g -o test test.o libabc.a -lm -ldl -lpthread
 cp test ~/build/run/bins
 cp ~/OpenABC/bench_openabcd/fpu_orig.bench ~/build/run/data
 cp abc.sh ~/build/run
