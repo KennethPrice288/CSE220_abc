3dec626005980128c077ec7aeae0801f431ca160
Kenneth Price <krprice@ucsc.edu>
2023-11-16 16:18:48 -0800

    Fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ make libabc.a ABC_USE_NO_READLINE=1
 riscv64_linux_gnu_gcc -static -Wall -g -c test.c -o test.o
 riscv64_linux_gnu_g++ -static -g -o test test.o libabc.a -lm -ldl -lpthread
 cp test ~/build/run/bins
-cp ~/OpenABC/bench/openabcd/fpu_orig.bench ~/build/run/data
+cp ~/OpenABC/bench_openabcd/fpu_orig.bench ~/build/run/data
 cp abc.sh ~/build/run
